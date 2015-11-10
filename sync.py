d6b3c56d2d71e917c76954515ca5bb70a25f24b6
Wrycu <blah380@gmail.com>
2015-11-09 21:57:31 -0500

    Further upload improvements

--- sync.py
+++ sync.py
@@ -1,6 +1,5 @@
-__author__ = 'Owner'
 from os import walk, remove
-from hashlib import sha1
+from hashlib import md5
 from subprocess import PIPE, Popen
 from ConfigParser import ConfigParser
 import requests
@@ -12,35 +11,16 @@ secret_obj = ConfigParser()
 secret_obj.read('a3sync_secret.cfg')
 conf = {}
 conf['include'] = conf_obj.get('directories', 'include').split(',')
-conf['digest'] = conf_obj.get('last_run', 'digest')
 filename = conf_obj.get('config', 'file_name')
 conf['client_secret'] = secret_obj.get('config', 'client_secret')
 conf['client_id'] = secret_obj.get('config', 'client_id')
 conf['access_token'] = secret_obj.get('config', 'access_token')
-conf['auth_code'] = secret_obj.get('config', 'auth_code')
+conf['file_id'] = secret_obj.get('config', 'file_id')
 
-# Value you get after navigating to
+# Navigate here to get the access token
 # https://accounts.google.com/o/oauth2/auth?client_id=600859841473-3o2savl23qfperh4t79paj8qp8qhpe8k.apps.googleusercontent.com&response_type=token&scope=https://www.googleapis.com/auth/drive&redirect_uri=http://leetsaber.com/callback
-# This code converts a token into an access token, but is no longer required because Google updated their API recently
-'''
-reply = requests.post(
-    'https://accounts.google.com/o/oauth2/token',
-    headers={'Content-Type': 'application/x-www-form-urlencoded'},
-    data='code=' + conf['auth_code'] +
-         '&redirect_uri=http://localhost'
-         '&client_id=' + conf['client_id'] +
-         '&client_secret=' + conf['client_secret'] +
-         '&grant_type=authorization_code'
-)
 
-print reply.text
-reply.raise_for_status()
-access_token = reply.json()['access_token']
-print access_token
-exit(0)
-'''
-
-
+matched = False
 directories = walk('.')
 command = [
     'rar',
@@ -48,12 +28,10 @@ command = [
     filename,
 ]
 
-matched = False
-
+# Walk the current directory to find the files we're supposed to upload
 for directory in directories:
     # os.walk returns the directory we walked prepended for whatever reason, so just substr it out
     the_dir = directory[0][2:]
-    print "'", the_dir, "'"
     if the_dir in conf['include']:
         print "Will archive", the_dir
         command.append(the_dir)
@@ -61,45 +39,55 @@ for directory in directories:
     elif directory[0] == '.':
         for the_file in directory[2]:
             if the_file == filename:
-                print "Found old", filename, "Deleting it..."
                 remove(filename)
-                print "Done!"
                 break
 
 if not matched:
-    print "Found no mod files. Quitting."
+    print "Found no matching files. Quitting."
     exit(1)
 
 # I'm so sorry.
-print "Creating archive..."
+print "Creating archive... (this might take a while)"
 stdout = Popen(command, stdout=PIPE).communicate()
 print "Done!"
 
-file_digest = sha1(filename).hexdigest()
-if file_digest == conf['digest']:
-    print "The archive we just created matches the previously created archive.  Quitting."
-    exit(1)
+# Get the digest of the previously uploaded file
+reply = requests.get(
+    'https://www.googleapis.com/drive/v2/files/{file}?access_token={token}'.format(
+        file=conf['file_id'],
+        token=conf['access_token'],
+    ),
+)
 
-# TODO
-# Write the current digest to the config file
-print "Uploading file"
+reply.raise_for_status()
+previous_digest = reply.json()['md5Checksum']
 
-# RARed the wrong files on windows.
-# TODO:
-# RAR the correct files
-reply = requests.post(
-    'https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart&access_token=' + conf['access_token'],
+# Generate the digest of the newly generated file and compare to avoid unnecessary work
+print "Calculating the digest of the new archive... (this might take a while)"
+file_digest = md5(open(filename, 'rb').read()).hexdigest()
+if file_digest == previous_digest:
+    print "The archive we just created matches the previously uploaded archive.  Quitting."
+    exit(2)
+
+# Actually upload the archive
+print "Uploading file... (this might take a while)"
+reply = requests.put(
+    'https://www.googleapis.com/upload/drive/v2/files/{file}?uploadType=multipart&access_token={token}'.format(
+        file=conf['file_id'],
+        token=conf['access_token'],
+    ),
     files={
         'file': open(filename, 'rb'),
         'data': (
             'metadata',
             DataDict(
-                title=filename
+                title=filename,
             ),
             'application/json; charset=UTF-8'
         )
     },
 )
 
-print reply.status_code
-print reply.text
+# Check the response for errors and print the URL (even though it shouldn't change)
+reply.raise_for_status()
+print "Uploaded! You can access the RAR at", reply.json()['webContentLink']
